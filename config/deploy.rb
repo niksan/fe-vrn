387ec3a8d6ef2e41bca80e2c026f6c7f334ac2a5
NikSan <nikulinaleksandr@gmail.com>
2013-07-15 15:49:24 +0400

    change db connection

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,12 @@ load 'deploy/assets'
 
 ssh_options[:forward_agent] = true
 
+set :keep_releases, 3
 set :application,     'fe-vrn'
-set :deploy_server,   "lithium.locum.ru"
+set :deploy_server,   'lithium.locum.ru'
 set :bundle_without,  [:development, :test]
-set :user,            "hosting_niksan"
-set :login,           "niksan"
+set :user,            'hosting_niksan'
+set :login,           'niksan'
 set :use_sudo,        false
 set :deploy_to,       "/home/#{user}/projects/#{application}"
 set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
@@ -15,15 +16,15 @@ set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
 set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
 role :web,            deploy_server
 role :app,            deploy_server
-role :db,             deploy_server, :primary => true
-set :rvm_ruby_string, "2.0.0-p0"
+role :db,             deploy_server, primary: true
+set :rvm_ruby_string, '2.0.0'
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 set :scm,             :git
-set :repository,      "git@github.com:niksan/fe-vrn.git"
+set :repository,      'git://github.com/niksan/fe-vrn.git'
 
-before "deploy:assets:precompile", :remove_paths, :set_links
-after "deploy:update_code", :do_migrations
+before 'deploy:assets:precompile', :remove_paths, :set_links
+after 'deploy:update_code', :do_migrations
 
 task :remove_paths, roles => :app do
   run "rm -rf #{release_path}/tmp"
@@ -31,8 +32,9 @@ end
 
 task :set_links, roles => :app do
   links = {
-    '/uploads' => '/public/uploads',
+    '/ckeditor_assets' => '/public/ckeditor_assets',
     '/system' => '/public/system',
+    '/uploads' => '/public/uploads',
     '/config/database.yml' => '/config/database.yml'
   }
   links.each do |from, destination|
@@ -46,25 +48,25 @@ task :do_migrations, roles => :app do
 end
 
 before 'deploy:finalize_update', 'set_current_release'
-task :set_current_release, :roles => :app do
+task :set_current_release, roles: :app do
     set :current_release, latest_release
 end
 
   set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 namespace :deploy do
-  desc "Start application"
-  task :start, :roles => :app do
+  desc 'Start application'
+  task :start, roles: :app do
     run unicorn_start_cmd
   end
 
-  desc "Stop application"
-  task :stop, :roles => :app do
+  desc 'Stop application'
+  task :stop, roles: :app do
     run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
   end
 
-  desc "Restart Application"
-  task :restart, :roles => :app do
+  desc 'Restart Application'
+  task :restart, roles: :app do
     run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
