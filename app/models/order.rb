fba95c7df0b46fc20a7996a4ee7a97811627fd9b
NikSan <nikulinaleksandr@gmail.com>
2013-08-05 23:02:41 +0400

    fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  attr_reader :name
   attr_accessible :additional, :address, :first_name, :last_name, :phone, :email
   validates :first_name, :last_name, :phone, :address, presence: true
 
@@ -12,6 +13,10 @@ class Order < ActiveRecord::Base
       transition all => :finished
     end
 
+  end
+
+  def name
+    self.class.model_name.human + " #" + id.to_s
   end
 
 end
