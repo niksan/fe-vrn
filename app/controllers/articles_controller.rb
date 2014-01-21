fb2438331c2962dc523aded483fed43cd3e15cd9
NikSan <nikulinaleksandr@gmail.com>
2014-01-21 15:01:31 +0400

    upd seo title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
+
   def show
     @article = Article.find params[:id]
+    @meta_tag_content[:title] = @article.title
   end
+
+  private
+
+    def set_seo_data
+    end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find params[:id]
+    @meta_tag_content[:title] = @page.title
   end
 end
