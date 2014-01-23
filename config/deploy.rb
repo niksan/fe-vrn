463a411db43e576456fbb8b7a7cd79fbd1791d9e
NikSan <nikulinaleksandr@gmail.com>
2014-01-23 14:02:16 +0400

    upd

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,15 +6,16 @@ set :rvm_ruby_string,      '2.0.0-p353@rails4'
 set :deploy_to,            "/srv/htdocs/#{fetch(:application)}"
 set :unicorn_conf,         "#{fetch(:deploy_to)}/current/config/unicorn.rb"
 set :unicorn_pid,          "#{fetch(:deploy_to)}/shared/pids/unicorn.pid"
-set :bundle_cmd,           "rvm use #{fetch(:rvm_ruby_string)} do bundle install"
+set :bundle_cmd,           "cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)} do bundle exec install"
+set :migrate_cmd,          "cd #{fetch(:deploy_to)}/current; RAILS_ENV=#{fetch(:rails_env)} rvm use #{fetch(:rvm_ruby_string)} do bundle exec install"
 set :format,               :pretty
 set :log_level,            :debug
 set :pty,                  true
 set :linked_files,         %w{config/database.yml}
 set :linked_dirs,          %w{bin log vendor/bundle public/system public/uploads}
 set :keep_releases,        5
-
-set :unicorn_start_cmd,    "(cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)} do bundle exec unicorn_rails -Dc #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)})"
+set :unicorn_start_cmd, "(cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)} do bundle exec unicorn_rails -Dc #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)})"
+set :unicorn_stop_cmd, "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
 
 namespace :deploy do
 
@@ -28,15 +29,17 @@ namespace :deploy do
   desc 'Stop application'
   task :stop do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
+      execute fetch(:unicorn_stop_cmd)
     end
   end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute ""
-      execute "cd #{fetch(:deploy_to)}/current; #{fetch(:bundle_cmd)}; [ -f #{fetch(:unicorn_pid)} ] && kill -USR2 `cat #{fetch(:unicorn_pid)}`"
+      execute fetch(:unicorn_stop_cmd)
+      execute fetch(:unicorn_bundle_cmd)
+      execute fetch(:unicorn_migrate_cmd)
+      execute fetch(:unicorn_start_cmd)
     end
   end
 
