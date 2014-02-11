5a657943530181ee17216d7ccf0f18120223def5
NikSan <nikulinaleksandr@gmail.com>
2014-02-11 10:52:17 +0400

    upd seo title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find params[:id]
-    @meta_tag_content[:title] = @meta_tag_content[:title_prefix] + @page.title
+    @meta_tag_content[:title] = @page.title
     @meta_tag_content[:keywords] = @page.title.split.join(' ')
   end
 end
